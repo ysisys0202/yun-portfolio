bed4ad43aec4bf9ce09e94436404b3438f9f72b4
이윤슬 <ysisys0202@gmail.com>
2025-03-22 00:08:21 +0900

    ✨ intro section 추가

--- assets/apps/index.js
+++ assets/apps/index.js
@@ -1,1 +1,15 @@
+import TextEffect from "./textEffect.js";
 
+const intro = document.querySelector(".intro");
+const introTitle = new TextEffect(intro.querySelector(".intro-title"));
+
+const introTitleChars = document.querySelectorAll(".intro-title .char");
+const lastIntroTitleChars = introTitleChars[introTitleChars.length - 1];
+
+window.addEventListener("load", () => {
+  introTitle.bounce();
+});
+
+lastIntroTitleChars.addEventListener("animationend", () => {
+  intro.classList.add("is-loaded");
+});
---
+++ assets/apps/textEffect.js
@@ -0,0 +1,30 @@
+export default class TextEffect {
+  constructor(element) {
+    this.element = element;
+    this.text = this.element.innerHTML.trim();
+    this.element.innerHTML = "";
+    this.splitText();
+  }
+
+  splitText() {
+    const regex = /(<br\s*\/?>|.)/g;
+    const textArray = this.text.match(regex).map((char) => {
+      return char === " " ? "&nbsp;" : char;
+    });
+    textArray.forEach((char, index) => {
+      if (char.startsWith("<")) {
+        this.element.innerHTML += char;
+      } else {
+        const span = document.createElement("span");
+        span.classList.add("char");
+        span.style.animationDelay = `${index * 0.05}s`;
+        span.innerHTML = char;
+        this.element.appendChild(span);
+      }
+    });
+  }
+
+  bounce() {
+    this.element.classList.add("bounce");
+  }
+}
