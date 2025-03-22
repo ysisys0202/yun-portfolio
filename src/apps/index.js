1bcdec86b4488ab9bd92a7872038ee06ea3e9585
이윤슬 <ysisys0202@gmail.com>
2025-03-22 23:27:00 +0900

    ✨ projects 기능 추가

--- src/apps/index.js
+++ src/apps/index.js
@@ -2,8 +2,10 @@ import { titleAnimation } from "./sections/common.js";
 import { introAnimation } from "./sections/intro.js";
 import { aboutAnimation } from "./sections/about.js";
 import { skillsAnimation } from "./sections/skills.js";
+import { projectsAnimation } from "./sections/projects.js";
 
 introAnimation();
 titleAnimation();
 aboutAnimation();
 skillsAnimation();
+projectsAnimation();
---
+++ src/apps/sections/projects.js
@@ -0,0 +1,64 @@
+import { gsap } from "gsap";
+
+export const projectsAnimation = () => {
+  gsap.to("body", {
+    scrollTrigger: {
+      trigger: ".projects",
+      start: "top 80%",
+      end: "bottom center",
+      onEnter: () => {
+        document.body.classList.add("bg-blue");
+      },
+      onLeave: () => {
+        document.body.classList.remove("bg-blue");
+      },
+      onEnterBack: () => {
+        document.body.classList.add("bg-blue");
+      },
+      onLeaveBack: () => {
+        document.body.classList.remove("bg-blue");
+      },
+    },
+  });
+
+  gsap.utils.toArray(".project-box").forEach((projectBox) => {
+    gsap.fromTo(
+      projectBox,
+      { scale: 0.8 },
+      {
+        scale: 1,
+        scrollTrigger: {
+          trigger: projectBox,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+          onUpdate: (self) => {
+            const progress = self.progress;
+
+            let scale;
+            let opacity;
+
+            if (progress < 0.2) {
+              scale = gsap.utils.mapRange(0, 0.2, 0.8, 1, progress);
+              opacity = gsap.utils.mapRange(0, 0.2, 0, 1, progress);
+            } else if (progress > 0.7) {
+              scale = gsap.utils.mapRange(0.7, 1, 1, 0.8, progress);
+              opacity = gsap.utils.mapRange(0.7, 1, 1, 0.6, progress);
+            } else {
+              scale = 1;
+              opacity = 1;
+            }
+
+            gsap.to(projectBox, {
+              scale,
+            });
+
+            gsap.to(projectBox.querySelector(".project-box-content"), {
+              opacity,
+            });
+          },
+        },
+      }
+    );
+  });
+};
