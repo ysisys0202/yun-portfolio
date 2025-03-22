092e6642b2c82a3bcdb2f9125e227805a1d5ac46
이윤슬 <ysisys0202@gmail.com>
2025-03-22 22:33:26 +0900

    ✨ about 애니메이션 추가

--- src/apps/index.js
+++ src/apps/index.js
@@ -1,5 +1,7 @@
-import { introAnimation } from "./sections/intro.js";
 import { titleAnimation } from "./sections/common.js";
+import { introAnimation } from "./sections/intro.js";
+import { aboutAnimation } from "./sections/about.js";
 
 introAnimation();
 titleAnimation();
+aboutAnimation();
---
+++ src/apps/sections/about.js
@@ -0,0 +1,21 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export const aboutAnimation = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.utils.toArray(".about .fadein-item").forEach((fadeinItem) => {
+    gsap.fromTo(
+      fadeinItem,
+      { y: 20, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 0.4,
+        scrollTrigger: {
+          trigger: fadeinItem,
+          start: "top 60%",
+        },
+      }
+    );
+  });
+};
