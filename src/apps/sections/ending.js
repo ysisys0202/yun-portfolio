651c7535c1ccf4baa6b5fa0f666087d2610a1ae2
이윤슬 <ysisys0202@gmail.com>
2025-03-23 23:56:38 +0900

    💫 ending 애니메이션 수정

--- src/apps/sections/ending.js
+++ src/apps/sections/ending.js
@@ -4,6 +4,26 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 export const endingAnimation = () => {
   gsap.registerPlugin(ScrollTrigger);
 
+  gsap.to("body", {
+    scrollTrigger: {
+      trigger: ".ending",
+      start: "top 60%",
+      end: "bottom center",
+      onEnter: () => {
+        document.body.classList.add("bg-blue");
+      },
+      onLeave: () => {
+        document.body.classList.remove("bg-blue");
+      },
+      onEnterBack: () => {
+        document.body.classList.add("bg-blue");
+      },
+      onLeaveBack: () => {
+        document.body.classList.remove("bg-blue");
+      },
+    },
+  });
+
   gsap.fromTo(
     ".ending.fadein-item",
     { opacity: 0, y: 20 },
